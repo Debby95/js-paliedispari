4e830af61b4b1266c93b3d75410542380d7cc5c5
Debby95 <debbyjacksonstar@hotmail.it>
2022-05-26 22:03:02 +0200

    stabilisce la vittoria

--- pari e dispari/app.js
+++ pari e dispari/app.js
@@ -3,14 +3,66 @@ const scelta = prompt("Scegli tra pari e dispari");
 //chiedo all utente di inserire un numero da 1 a 5
 const numero = prompt("Inserisci un numero da 1 a 5");
 
-let numeroRandomPc = Math.random();
+let somma = 0;
+let numeroRandomPc = 0;
+let risultato = 0;
 
-function generoNumeroRandom() {
-    for (let i = 0; i <= 5; i++){
-        numeroRandomPc = Math.floor(numeroRandomPc * 5);
+//funzione per generare numero random
+function numeroRandom() {
+    const numeroRandomPc = (Math.floor(Math.random() * 5) + 1);
+    //console.log(numeroRandomPc);
+    return numeroRandomPc;
+}
+
+//funzione per sommare i due numeri generati
+function sommaNumeri (numero, numeroRandomPc) {
+    numeroRandomPc = numeroRandom();
+    //console.log(numero);
+    console.log(numeroRandomPc);
+    const risultato = parseInt(numero) + parseInt(numeroRandomPc);
+    console.log(risultato);
+    return risultato;
+}
+
+//sommaNumeri(numero, numeroRandomPc);
+
+//console.log(scelta);
+//console.log(numero);
+
+//stabilisco se la somma dei due numeri è pari o dispari
+function isPari(risultato, risultatoPariDispari) {
+    risultato = sommaNumeri(numero, numeroRandomPc);
+    if (risultato % 2 === 0) {
+        risultatoPariDispari = "pari";
+        console.log("è pari");
+    } else {
+        risultatoPariDispari = "dispari";
+        console.log("è dispari");
+    }
+    console.log(risultato);
+    
+    return risultatoPariDispari;
+}
+
+isPari(risultato);
+
+
+//stabilisco la vittoria
+function vittoria(risultatoPariDispari) {
+    risultatoPariDispari = isPari(risultato, risultatoPariDispari);
+    if (scelta === risultatoPariDispari) {
+        console.log("HAI VINTO");
+    } else {
+        console.log("HAI PERSO");
     }
 }
 
-console.log(scelta);
-console.log(numero);
-console.log(numeroRandomPc);
+vittoria();
+
+
+
+
+
+
+
+
